b6ef75820fc2c5316183202c6083641f07505ca7
Tamara <86376787+tamaradg@users.noreply.github.com>
2021-09-09 17:02:30 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ const baseUrl = "https://teclead.de/recruiting/radios";
 const radio = document.querySelector(".radio");
 const results = document.querySelector(".result");
 
-radio.addEventListener("click", (event) => {
-  fetch(baseUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      data.radios.forEach((radio) => {
-        results.insertAdjacentHTML("beforeend", `<li>${radio.image}</li>`);
-      });
+fetch(baseUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    data.radios.forEach((r) => {
+      radio.insertAdjacentHTML(
+        "beforeend",
+        `<div class="border-bottom">
+          <div class="radio-detail">
+            <button>${r.name} </button>
+            <p>${r.frequency}</p>
+          </div>
+        </div>
+        <div><img class="fit-picture" src="${r.image}"</div>`
+      );
     });
-});
+  });
