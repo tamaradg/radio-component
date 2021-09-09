1c71ab29d27f1b1578be44f31530e06665e6c221
Tamara <86376787+tamaradg@users.noreply.github.com>
2021-09-09 15:17:12 +0200

    js addeventListener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles.css";
 
-document.getElementById("card").innerHTML = "";
+const baseUrl = "https://teclead.de/recruiting/radios";
+const radio = document.querySelector(".radio");
+const results = document.querySelector(".result");
+
+radio.addEventListener("click", (event) => {
+  fetch(baseUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      data.radios.forEach((radio) => {
+        results.insertAdjacentHTML("beforeend", `<li>${radio.image}</li>`);
+      });
+    });
+});
